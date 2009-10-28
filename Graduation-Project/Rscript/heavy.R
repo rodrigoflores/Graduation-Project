f22d9111c9e0fd00f1a53db17bf57d2a13903ee3
Rodrigo L. M. Flores <rlmflores@src.gnome.org>
2009-10-28 19:09:46 -0200

    Some examples added

---
+++ Graduation-Project/Rscript/heavy.R
@@ -0,0 +1,14 @@
+
+x = rnorm(100000)
+y = rnorm(100000)
+z = x + y
+
+p <- scan("random.txt", list(0))
+t = p[[1]]
+g = c()
+for(i in c(1:length(t))) 
+{
+  g = c(g,max(t[i],z))
+}
+l = c(mean(g))
+write(l,"")
