055696f67b0dab5e76038aab50842dbbe963fc8c
Rodrigo Flores <rodrigo.flores@sabbre.com.br>
2010-02-03 20:38:54 -0200

    adicionado .dat do arquivo

--- data/conversor.rb
+++ data/conversor.rb
@@ -1,13 +1,16 @@
 #!/usr/bin/ruby -w
 
-file = File.open("db9.txt")
+file = File.open("result5.csv")
 
 while(line = file.gets) do
-  if line =~ /^(test1.+),(.+),(.+),(.+)$/
+  if line =~ /^(.+),(.+),(.+),(.+),(.+)$/
     name = $1
-    received_time = $2.to_i
-    sent_time = $3.to_i
-    printf "%s\t%s\t%s\t%d\n", name,sent_time,received_time,-1*(received_time-sent_time)
+    received_time = $3.to_i
+    sent_time = $4.to_i
+    computer = $5.to_i
+    if name =~ /test_valendo2/
+      printf "%s\t%d\t%d\t%d\t%d\n", name,sent_time,received_time,(sent_time-received_time),computer
+    end
   end
 end
 
