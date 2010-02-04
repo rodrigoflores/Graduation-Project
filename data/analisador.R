503864d4239e3bda485e5eb63f0d02aa1faf62b4
Rodrigo L. M. Flores <rlmflores@gnome.org>
2010-02-03 23:11:18 -0200

    Adicionado um analisador

---
+++ data/analisador.R
@@ -0,0 +1,36 @@
+table = read.table("saida2.dat")
+
+mean(table[[4]])
+sd(table[[4]])
+min(table[[2]]) - max(table[[3]])
+
+(300*mean(table[[4]]))
+
+l = c()
+z = table[[4]]
+y = table[[5]]
+k = table[[3]]
+m = table[[2]]
+for(i in 1:300)
+{
+  if(y[i] == 26)
+  {
+    l = c(l,TRUE)
+  }
+  else
+  {
+    l = c(l,FALSE)
+  }
+}
+
+mean(z[l])
+sd(z[l])
+300.00*mean(z[l])
+min(m[l]) - max(k[l])
+
+
+mean(z[!l])
+sd(z[!l])
+300*mean(z[!l])
+min(m[!l]) - max(k[!l])
+
